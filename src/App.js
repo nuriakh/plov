d8d5dc683e429bf813ded647e807155a02c983c6
nuriakh <101659162+nuriakh@users.noreply.github.com>
2024-02-05 00:28:33 +0100

    bulb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {createContext, useState} from "react"
+import Lamp from './Lamp.jsx' 
+import ReactSwitch from 'react-switch';
+export const ThemeContext  = createContext(null)
+
 
 function App() {
+  const [theme,setTheme] = useState("dark")
+  const toggleTheme = () =>{
+    setTheme((curr)=> (curr === "light" ? "dark" : "light"))
+  }
   return (
-    <div className="App">
+   <>
+   <ThemeContext.Provider value={{theme, toggleTheme}}>
+   <div className="App" id = {theme}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <div >
+          <div>
+          <label className='textSwitch'>{theme ==="dark" ? "Turn on the light  " : "Thanks  "}</label>
+          </div>
+        <ReactSwitch className='switch' onChange={toggleTheme} checked={theme==="light"}/>
+        <Lamp/>
+        </div>
       </header>
     </div>
+    </ThemeContext.Provider>
+    <div className='one'>
+    <header className="one-header">
+      <p>
+        hui
+      </p>
+      <div className='lamp'>
+        
+      </div>
+      </header>
+    </div>
+    </>
   );
 }
 
---
+++ src/Lamp.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Lamp extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className='wrapper'>
+        <div className='light'>
+         <div className='bulb'>
+         <span></span>
+         <span></span>
+         </div>
+        </div>
+        </div>
+        
+       
+
+      </div>
+    );
+  }
+}
+
+export default Lamp;
